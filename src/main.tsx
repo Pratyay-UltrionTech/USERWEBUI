acf2a9cb4419dc72def0f3c334ea979be412e62a
Pratyay-UltrionTech <pratyay@ultriontech.com>
2026-04-20 19:49:17 +0530

    updated code9

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import { createRoot } from "react-dom/client";
 import { jsx } from "react/jsx-runtime";
 import App from "./app/App.tsx";
 import "./styles/index.css";
-
+ 
 createRoot(document.getElementById("root")!).render(jsx(App, {}));
-  
+        
